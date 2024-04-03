ae97a3143489922e3e96c3f89758f055c3ac9285
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-04-03 11:01:53 +0900

    first commit

---
+++ src/main/java/ultimatum/project/UltimatumApplication.java
@@ -0,0 +1,13 @@
+package ultimatum.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimatumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimatumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ultimatum/project/UltimatumApplicationTests.java
@@ -0,0 +1,13 @@
+package ultimatum.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimatumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
