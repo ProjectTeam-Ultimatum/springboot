05ef85567c636475afd89ec2581b99578b3510e2
이은재 <>
2024-04-03 11:45:17 +0900

    test commit

--- src/main/java/ultimatum/project/UltimatumApplication.java
+++ src/main/java/ultimatum/project/UltimatumApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UltimatumApplication {
-//test
+//test...
     public static void main(String[] args) {
         SpringApplication.run(UltimatumApplication.class, args);
     }
