07a0d7fbea883b53b65090233d5b5c011d8f2155
Ruah Kim <kimhk0315@gmail.com>
2024-04-09 14:50:08 +0900

    댓글 삭제 완료

--- src/main/java/ultimatum/project/dto/reviewReplyDTO/UpdateReplyResponse.java
+++ src/main/java/ultimatum/project/dto/reviewReplyDTO/UpdateReplyResponse.java
@@ -3,7 +3,6 @@ package ultimatum.project.dto.reviewReplyDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import ultimatum.project.domain.entity.review.Review;
 
 @Getter
 @NoArgsConstructor
--- src/main/java/ultimatum/project/service/review/ReviewReplyService.java
+++ src/main/java/ultimatum/project/service/review/ReviewReplyService.java
@@ -1,6 +1,5 @@
 package ultimatum.project.service.review;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
