635cdb780e4e9fc1e348935567bece9c15ce5c02
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-04-15 19:07:55 +0900

    [feature] LonIn API  vue 호출

--- src/main/java/ultimatum/project/global/config/CorsConfig.java
+++ src/main/java/ultimatum/project/global/config/CorsConfig.java
@@ -6,21 +6,38 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+//@Configuration
+//public class CorsConfig {
+//
+//   @Bean
+//   public CorsFilter corsFilter() {
+//      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//      CorsConfiguration config = new CorsConfiguration();
+//      config.setAllowCredentials(true);
+//      config.addAllowedOrigin("*");
+//      config.addAllowedHeader("*");
+//      config.addAllowedMethod("*");
+//
+//      source.registerCorsConfiguration("/api/**", config);
+//      source.registerCorsConfiguration("/login_proc", config);
+//      return new CorsFilter(source);
+//   }
+//
+//}
 @Configuration
 public class CorsConfig {
 
    @Bean
    public CorsFilter corsFilter() {
-      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowCredentials(true);
-      config.addAllowedOrigin("*");
+      config.addAllowedOriginPattern("*"); // 실제 배포시에는 구체적인 도메인으로 제한하세요
       config.addAllowedHeader("*");
+      config.addExposedHeader("Authorization"); // 이 부분이 중요
       config.addAllowedMethod("*");
 
-      source.registerCorsConfiguration("/api/**", config);
-      source.registerCorsConfiguration("/login_proc", config);
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", config);
       return new CorsFilter(source);
    }
-
 }
