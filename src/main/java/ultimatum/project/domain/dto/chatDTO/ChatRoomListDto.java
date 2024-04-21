39fae19a87f63675e2a7f4c72f95ebc0eeb61250
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-04-21 21:06:22 +0900

    [MERGE] 일요일 오전 2차 병합

--- src/main/java/ultimatum/project/domain/dto/chatDTO/ChatRoomListDto.java
+++ src/main/java/ultimatum/project/domain/dto/chatDTO/ChatRoomListDto.java
@@ -3,7 +3,6 @@ package ultimatum.project.domain.dto.chatDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
 @Data
 @AllArgsConstructor
