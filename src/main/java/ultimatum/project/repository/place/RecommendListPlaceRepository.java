a0018b9890067a95710d75ebe61f62585d275881
Yirang Kim <olrang.2@gmail.com>
2024-05-24 17:55:30 +0900

    💡 Feat: "Recommend JpaRepository"
    주석 추가
    
    Resolves: #92

--- src/main/java/ultimatum/project/repository/place/RecommendListPlaceRepository.java
+++ src/main/java/ultimatum/project/repository/place/RecommendListPlaceRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ultimatum.project.domain.entity.place.RecommendListPlace;
 
 public interface RecommendListPlaceRepository extends JpaRepository<RecommendListPlace, Long> {
+    // 특정 태그를 포함하는 관광지 목록을 페이지 단위로 반환
     Page<RecommendListPlace> findByRecommendPlaceTagContainingIgnoreCase(String tag, Pageable pageable);
+    // 특정 지역을 포함하는 관광지 목록을 페이지 단위로 반환
     Page<RecommendListPlace> findByRecommendPlaceRegionContainingIgnoreCase(String region, Pageable pageable);
+    // 특정 제목를 포함하는 관광지 목록을 페이지 단위로 반환
     Page<RecommendListPlace> findByRecommendPlaceTitleContainingIgnoreCase(String title, Pageable pageable);
+    // 특정 ID를 가진 관광지 정보를 반환
     RecommendListPlace findByRecommendPlaceId(Long id); //ReplyService 관광지 평점 등록
 }
